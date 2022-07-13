24a3d21a07c52f729af93d0879bfebca22e7794e
Maksim Tuchka <maksim.tuchka@yahoo.com>
2022-07-13 08:57:26 +0300

    Version 3

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -7,7 +7,7 @@ from loader import dp
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = ("Список команд: ",
-            "/start - Начать диалог",
+            "/start - Начать диалог1",
             "/help - Получить справку")
     
     await message.answer("\n".join(text))
